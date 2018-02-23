a187b2eeb2133d16a052e22181af836269ce9439
Sarah <sarah@github.com>
2018-02-23 17:48:52 -0500

    Initial commit

---
+++ includes/navigation.php
@@ -0,0 +1,16 @@
+<header>
+<div class = "logo"><h1><a href="index.php"><img src="images/logo.png" alt ="Logo"> 
+<br>Medellin Inc</h1> </a> 
+	<strong>CSC 174 - Advanced Front End Web Design </strong></div>
+<hr>
+</header>
+
+
+<nav class = "menu">
+	<ul>
+		<li><a href="index.php">Home Page</a></li>
+		<li><a href="casey.php">Casey Brentnall</a></li>
+		<li><a href="irfan.php">Irfan Bashir</a></li>
+		<li><a href="jimmy.php">Jimmy Yang</a></li>
+	</ul>
+</nav>
